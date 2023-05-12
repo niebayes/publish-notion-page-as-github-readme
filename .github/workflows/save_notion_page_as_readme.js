6e0656392d2f61e49f152d9a6cb7c00a04de71f4
niebayes <niebayes@gmail.com>
2023-05-12 15:23:34 +0800

    Update and rename fetch_notion_page.js to save_notion_page_as_readme.js

---
+++ .github/workflows/save_notion_page_as_readme.js
@@ -0,0 +1,29 @@
+const { Client } = require("@notionhq/client");
+const { NotionToMarkdown } = require("notion-to-md");
+const fs = require('fs');
+
+const NOTION_API_KEY = 'secret_DgYm1Y6FKbGmyP2q7pPcs0ivFunyBbjJFbusrYXrXuG'
+const pageId = '1a15db43a4594971b99fbcfb565d2c9d'
+const filePath = `README.md`;
+
+async function main() {
+    try {
+        const notion = new Client({
+            auth: NOTION_API_KEY,
+        });
+
+        // passing notion client to the option
+        const n2m = new NotionToMarkdown({ notionClient: notion });
+
+        const mdblocks = await n2m.pageToMarkdown(pageId);
+        const mdString = n2m.toMarkdownString(mdblocks);
+        fs.writeFileSync(filePath, mdString.parent);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+console.log("fetching page...");
+main();
+console.log("done");
